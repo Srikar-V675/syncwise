afe68151370673b91e512fde5def6730c4fc6a32
Srikar V <srikarvuchiha@gmail.com>
2024-11-11 19:40:44 +0530

    Added User and Student APIs 1. User creates and assigns the group `Students` and the department from authenticated user(i.e Teacher)

--- syncwise/urls.py
+++ syncwise/urls.py
@@ -4,11 +4,12 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from gradesync.views import UserViewSet
+from gradesync.views import StudentViewSet, UserViewSet
 
 # Create a router and register our viewset with it.
 router = DefaultRouter()
-router.register(r"users", UserViewSet)
+router.register(r"users", UserViewSet, basename="user")
+router.register(r"students", StudentViewSet, basename="student")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
